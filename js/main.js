d07198525c7dbaf2f4701bed55fbaa20b76e2e1c
kdxiaoyi <kdxiaoyi@outlook.com>
2024-08-20 20:45:28 +0800

    新增：基于COPS Policy限制的图片数量获取 新增：现在可以完整显示车牌号对应的图片组了 新增：现在可以通过AppBar直达client.html 修改：修改了404.html 新增：一些免责声明 移除：删掉了一些提示 新增：新增pixiv.svg和图片错误的代替图片imgErr.jpg

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function removeElement(ElementId) { document.getElementById(ElementId).remove();
 function author_kdxiaoyi() {let a = document.createElement("a");a.setAttribute("href", 'https://github.com/kdxhub/pixiv.re_ui');a.setAttribute("target", "_blank");a.click();a.remove();return;};
 function msg(Message, confirmTips) {if ( confirmTips == undefined ) {snackbar.innerHTML = Message;} else {snackbar.innerHTML = Message+`<s-button type="text" slot="action"> `+confirmTips+` </s-button>`;};snackbar.show();return;};//封装了一个Material Design的Tipbox
 function setUItitle(Title) {if (Title == undefined) {document.getElementById("titlebar").innerHTML="Pixiv.re UI"} else {document.getElementById("titlebar").innerHTML=Title};return;};
-function setPageTitle(Title) {if (Title == undefined) {document.getElementById("pagetitle").innerHTML="Pixiv.re UI - kdx233.github.io";} else {document.getElementById("pagetitle").innerHTML=Title+` - Pixiv.re UI - kdx233.github.io`;};return;};
+function setPageTitle(Title) {if (Title == undefined) {document.getElementById("pagetitle").innerHTML="Pixiv.re UI - kdxiaoyi.top";} else {document.getElementById("pagetitle").innerHTML=Title+` - Pixiv.re UI - kdxiaoyi.top`;};return;};
 function newSearch() {//点击「发起新请求」
   document.getElementById('new_id').value='';
   document.querySelector('#newInput').show();
@@ -17,22 +17,50 @@ function startsearch(id) {//发起新请求
   if (nl.selected == true) {proxy="https://pixiv.nl/";};
   if (re.selected == true) {proxy="https://pixiv.re/";};
   if (cat.selected == true) {proxy="https://pixiv.cat/";};
+  console.log(`发起新请求 @ `+window.location.href.replace(/[?&].*=[^&]*&?/g, "") + "?id=" + id.replace(/[^0-9]/g, "") + "&proxy=" +proxy);
   window.location.href = window.location.href.replace(/[?&].*=[^&]*&?/g, "") + "?id=" + id.replace(/[^0-9]/g, "") + "&proxy=" +proxy;
   return;
 };
-function search(id) {//开始请求指定图片
+async function search(id) {//开始请求指定图片
   if (Number(id.replace(/[^0-9]/g, "")) <= 0) {msg("主人，这是一个无效的车牌号喵~", "Σ(ﾟ∀ﾟﾉ)ﾉ");newSearch();return;};
-  setUItitle(`<s-button disabled="true" type="text"><s-circular-progress slot="start" indeterminate="true"></s-circular-progress></s-button>`+id);
+  console.log("开始请求指定图片%"+id);
+  setUItitle(`<s-icon><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 25 2 C 12.317 2 2 12.317 2 25 C 2 37.683 12.317 48 25 48 C 37.683 48 48 37.683 48 25 C 48 12.317 37.683 2 25 2 z M 26.300781 12.009766 C 35.827781 12.009766 39.291969 18.146531 39.292969 23.269531 C 39.292969 28.539531 34.961781 33.662109 26.300781 33.662109 C 23.332781 33.662109 20.992094 32.832375 19.371094 31.984375 L 19.371094 36.259766 L 20.238281 36.259766 L 20.238281 37.992188 L 15.041016 37.992188 L 15.041016 36.259766 L 15.90625 36.259766 L 15.90625 17.945312 C 14.85625 18.643313 13.308594 20.669922 13.308594 20.669922 L 13.308594 22.070312 L 12.443359 22.070312 L 10.710938 18.939453 C 10.710938 18.939453 16.773781 12.009766 26.300781 12.009766 z M 26.300781 14.164062 C 23.557781 14.164062 21.249094 14.824656 19.371094 15.722656 L 19.371094 29.779297 C 20.524094 30.293297 22.829781 31.064453 26.300781 31.064453 C 31.939781 31.064453 34.962891 26.733531 34.962891 23.269531 C 34.962891 18.920531 32.050781 14.164062 26.300781 14.164062 z"></path></svg></s-icon> `+id);
   setPageTitle(id);
-  msg("抱歉，相关功能开发喵还在掉头发……","凭啥不支持跨域请求！！！")
+  welcome.style=hide;result.style=unhide;
+  document.getElementById("img1").src=proxy+id+`.png`;
+    console.log(`Image#`+maxIndex+` 已发送请求\n出现CORS Policy报错即请求完毕，但Javascript无法捕获此错误。`);
+  while (true) {
+    Index = Index + 1;
+    let response=await fetch(proxy+id+`-`+Index+`.png`, {});
+    //根据代理站点特性，文件存在可以被跨域请求，即await可以正确执行
+    // 而若文件不存在，即超出图片数量的请求，会被CORS Policy拦截
+    // 则await出错，js执行在上行代码处中断，maxIndex值无法被增加
+    maxIndex = maxIndex + 1;
+    resultList.innerHTML=resultList.innerHTML+`<li id="li`+maxIndex+`"><img id="img`+maxIndex+`" src="`+proxy+id+`-`+maxIndex+`.png`+`" onerror="img_err_callback(`+maxIndex+`);"></li>`;
+    console.log(`Image#`+maxIndex+` 已验证存在`);
+    maxTips.innerHTML = `喵~此图集包含`+maxIndex+`张图片`;
+  };
+};
+function img_err_callback(which) {//图片加载失败回调
+  msg(`图片#`+which+` 加载失败，也许由于网络原因或编号不存在，反正不是本喵的错误 哼╯^╰`,"(⋟﹏⋞)");
+  console.warn(`Image #`+which+` 加载失败`);
+  let img_onerr=document.getElementById("img"+which);
+  img_onerr.setAttribute("onerror","void(0);");//取消回调防止二次错误
+  img_onerr.src="./res/imgErr.jpg";
 };
 function download() {//下载图片
   
 };
 //定义变量与常量
 const hide="display:none;";const unhide="";
-var id=0;
+var id=0; //图片id
+var maxIndex=1; //最大图片编号
+var Index=1; //当前选定图片编号
 const snackbar=document.getElementById("snackbar");
+const welcome=document.getElementById("welcome");
+const result=document.getElementById("result");
+const resultList=document.getElementById("result-list");
+const maxTips=document.getElementById("maxTips")
 //proxy识别
 if (getQueryString('proxy') != null) {var proxy=getQueryString('proxy');} else {var proxy=`https://pixiv.re/`;};
 //代理源选项控件常量
@@ -40,7 +68,7 @@ const re=document.getElementById('re');
 const nl=document.getElementById('nl');
 const cat=document.getElementById('cat');
 //移除noscript标识
-console.log ("pixiv.re UI");console.log("Copyright (C) 2024 kdxiaoyi. All right reserved.\nSee more @ //kdx233.github.io/licen");
+console.log ("pixiv.re UI");console.log("Copyright (C) 2024 kdxiaoyi. All right reserved.\nSee more @ //kdx233.github.io/licen\n仅供个人学习交流，遵守当地法律法规，切勿用于违法用途。");
 document.getElementById("noscript").setAttribute("style", "display:none;");
 //若有id参数则发起请求尝试
 if ( getQueryString('id') != null ) {search(getQueryString('id'));};
--- readme.md
+++ readme.md
@@ -6,10 +6,10 @@
 你要让我怎么介绍.png<br>
 就是觉得pixiv.nl没有UI，不方便一键批量下载，所以才搞得简易ui<br>
 ~~CUI不需要写页面布局，H5布局抄一下就行，多快~~<br>
-~~也绝对不是因为pixiv.moe半挂了才一怒之下做的~~
+~~也绝对不是因为pixivel.moe半挂了才一怒之下做的~~
 
 # 使用
-访问我们的[在线使用页面](https://kdxhub.github.io/pixiv.re_ui/)或者[下载客户端](https://kdxhub.github.io/pixiv.re_ui/client.html)！
+访问我们的[在线使用页面](https://kdxiaoyi.top/pixiv.re_ui/)或者[下载客户端](https://kdxhub.github.io/pixiv.re_ui/client.html)！
 
 # 参考与来源
 本仓库基于AGPL 3.0协议开源。<br>
@@ -18,4 +18,3 @@
 本工程借鉴了以下来源，对此表示鸣谢。<br>
 若有侵权请联系删除<br>
 * [Soberjs UI库](//soberjs.com)
-* jQuery库（暂定）
